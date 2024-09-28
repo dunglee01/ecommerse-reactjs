6ec81f32049d0047b50e4e076ba05a66ce7081bb
dunglv <dunglvdeveloper@gmail.com>
2024-09-28 14:13:46 +0700

    update react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import Blog from '@components/Blog/Blog';
 import HomePage from '@components/HomePage/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import routers from '@/routers/routers';
+import { Suspense } from 'react';
 
 function App() {
     return (
-        <>
-            <HomePage />
-        </>
+        <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {routers.map((item, index) => {
+                        return (
+                            <Route
+                                path={item.path}
+                                element={<item.component />}
+                                key={index}
+                            />
+                        );
+                    })}
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
     );
 }
 
---
+++ src/routers/routers.js
@@ -0,0 +1,14 @@
+import { lazy } from 'react';
+
+const routers = [
+    {
+        path: '/',
+        component: lazy(() => import('@components/HomePage/HomePage'))
+    },
+    {
+        path: '/blog',
+        component: lazy(() => import('@components/Blog/Blog'))
+    }
+];
+
+export default routers;

b341a63adfe534b89d6a3d42a89ab1b57bb27221
dunglv <dunglvdeveloper@gmail.com>
2024-11-22 12:02:14 +0700

    update code video 38

--- src/pages/Cart/components/contents/Contents.jsx
+++ src/pages/Cart/components/contents/Contents.jsx
@@ -1,14 +1,33 @@
 import CartTable from '@/pages/Cart/components/contents/CartTable';
 import styles from '../../styles.module.scss';
 import CartSummary from '@/pages/Cart/components/contents/CartSummary';
+import Button from '@components/Button/Button';
 
 function Contents() {
-    const { containerContents } = styles;
+    const { containerContents, boxFooter, boxBtnDelete, boxCoupon } = styles;
 
     return (
         <div className={containerContents}>
-            <div>
+            <div
+                style={{
+                    width: '58%'
+                }}
+            >
                 <CartTable />
+
+                <div className={boxFooter}>
+                    <div className={boxCoupon}>
+                        <input type='text' placeholder='Coupon code' />
+                        <Button content={'OK'} isPriamry={false} />
+                    </div>
+
+                    <div className={boxBtnDelete}>
+                        <Button
+                            content={<div>&#128465; CLEAR SHOPPING CART</div>}
+                            isPriamry={false}
+                        />
+                    </div>
+                </div>
             </div>
 
             <CartSummary />
